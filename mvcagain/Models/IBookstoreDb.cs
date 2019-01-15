4905ef5c7ccb177fa2fba8265ee66b651aef0991
Nikolaos Diakos <Nikolaos.Diakos@Peoplecert.onmicrosoft.com>
2019-01-15 19:00:23 +0200

    Added procedures in IBookstoreDb

--- mvcagain/Models/IBookstoreDb.cs
+++ mvcagain/Models/IBookstoreDb.cs
@@ -4,6 +4,22 @@ namespace mvcagain.Models
 {
     public interface IBookstoreDb
     {
+        public void Create(Book x);
+
+        public void Create(Publisher x);
+
+
+        public void Update(Book x);
+
+
+        public void Update(Publisher x);
+
+
+        public void Delete(Book x);
+
+
+        public void Delete(Publisher x);
+
         IEnumerable<Publisher> GetPublishers();
     }
 }
